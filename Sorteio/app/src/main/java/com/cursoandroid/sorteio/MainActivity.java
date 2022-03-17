b5564eda0ee5b8179f353874b7b6346990e1ccd7
Andre Akira <aah950@hotmail.com>
2022-03-17 11:18:40 -0300

    sec. 6: app sorteio
    finalizado app sorteio

--- Sorteio/app/src/main/java/com/cursoandroid/sorteio/MainActivity.java
+++ Sorteio/app/src/main/java/com/cursoandroid/sorteio/MainActivity.java
@@ -3,6 +3,10 @@ package com.cursoandroid.sorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sorteio(View view) {
+        TextView texto = findViewById(R.id.viewResultado);
+        int numero = new Random().nextInt(11);
+        texto.setText("Numero: " + numero);
+
     }
 }
