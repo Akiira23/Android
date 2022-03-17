5ebaaa54c8272cac172d70e0bfe55a0b4ddfbb64
Andre Akira <aah950@hotmail.com>
2022-03-17 11:41:11 -0300

    adicionado mais funcoes no sorteio
    sorteio com 2 jogadores, comparando seus valores

--- Sorteio/app/src/main/java/com/cursoandroid/sorteio/MainActivity.java
+++ Sorteio/app/src/main/java/com/cursoandroid/sorteio/MainActivity.java
@@ -17,9 +17,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sorteio(View view) {
+
         TextView texto = findViewById(R.id.viewResultado);
+        TextView vencedor = findViewById(R.id.viewVencedor);
+        TextView jog1 = findViewById(R.id.viewPlayer1);
+        TextView jog2 = findViewById(R.id.viewPlayer2);
+        int intJog1 = Integer.valueOf(jog1.getText().toString());
+        int intJog2 = Integer.valueOf(jog2.getText().toString());
         int numero = new Random().nextInt(11);
-        texto.setText("Numero: " + numero);
+        int resultado1 = Math.abs(intJog1 - numero);
+        int resultado2 = Math.abs(intJog2 - numero);
+
+        texto.setText("Numero sorteado: " + numero);
+        if (resultado1 < resultado2)
+            vencedor.setText("Jogador 1 venceu!");
+        else if (resultado2 < resultado1)
+            vencedor.setText("Jogador 2 venceu!");
+        else
+            vencedor.setText("Empate!");
 
     }
 }
