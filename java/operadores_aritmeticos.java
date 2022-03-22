820741b8e69e2c3c066bf01a29116cf947ae6f07
Andre Akira <aah950@hotmail.com>
2022-03-22 14:24:45 -0300

    variaveis e operadores

---
+++ java/operadores_aritmeticos.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        int numero1 = 10, numero2 = 5, resultado = 0;
+        resultado = numero1 + numero2;
+        System.out.println(resultado);
+        resultado = numero1 / numero2;
+        System.out.println(resultado);
+        resultado = numero1 % numero2;
+        System.out.println(resultado);
+    }
+
+}
---
+++ java/variavel.java
@@ -0,0 +1,20 @@
+package domain;
+
+public class variavel {
+
+    public static void main(String[] args) {
+        String nome = "Hello Zaworudo!";
+        int numero = 10, numero2 = 5;
+        
+        final int ALTURA = 187; //nao altera
+        
+        final double preco = 2.1451789;
+        boolean flag = true;
+        
+        System.out.println(nome);
+        System.out.println(numero + numero2);
+        System.out.println(preco);
+        System.out.println(flag);
+    }
+
+}
