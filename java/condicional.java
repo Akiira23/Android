ba570bcc003cd5a357e04200dd7e09f463721241
Andre Akira <aah950@hotmail.com>
2022-03-22 14:33:45 -0300

    condicionais

---
+++ java/condicional.java
@@ -0,0 +1,16 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int idade = 18;
+	    if(idade <= 12) {
+	        System.out.println("Crianca");
+	    }
+	    else if(idade < 18) {
+	        System.out.println("Adolescente");
+	    }
+	    else {
+	        System.out.println("Adulto");
+	    }
+		
+	}
+}
