51675298d437fb3d72c0298adf61f68072a1816c
Andre Akira <aah950@hotmail.com>
2022-03-22 14:37:18 -0300

    arrays

---
+++ java/arrays.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String[] nome = {"jamelao", "leticia"};
+	    int[] numeros = {20, 50, 80, 10};
+	    boolean[] itens = {true, false, false, true};
+	    
+	    System.out.println(nome[0]);
+	    System.out.println(numeros[2]);
+	    System.out.println(itens[1]);
+	}
+}
