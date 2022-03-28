3a718f1068493ec01ea58a5fc81ac7f63bf8b097
Andre Akira <aah950@hotmail.com>
2022-03-28 11:00:55 -0300

    app gorjeta alterado
    adicionado a funcao de dividir o total da conta de acordo com a quantidade de pessoas

--- CalculadoradeGorjeta/app/src/main/java/com/cursoandroid/calculadoradegorjeta/MainActivity.java
+++ CalculadoradeGorjeta/app/src/main/java/com/cursoandroid/calculadoradegorjeta/MainActivity.java
@@ -9,12 +9,13 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText editValor;
-    private TextView textPorcentagem;
-    private TextView textGorjeta;
-    private TextView textTotal;
+    private TextView textPorcentagem, textGorjeta, textTotal, textDividido;
+    private TextInputEditText textQuantidade;
     private SeekBar seekBarGorjeta;
 
     private double porcentagem = 0;
@@ -29,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         textGorjeta = findViewById(R.id.textGorjeta);
         textTotal = findViewById(R.id.textTotal);
         seekBarGorjeta = findViewById(R.id.seekBarGorjeta);
+        textDividido = findViewById(R.id.textDividido);
+        textQuantidade = findViewById(R.id.textQuantidade);
 
         seekBarGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -53,19 +56,26 @@ public class MainActivity extends AppCompatActivity {
     public void calcular() {
 
         String valorRecuperado = editValor.getText().toString();
-        if (valorRecuperado == null || valorRecuperado.equals("")) {
+        String quantidadeRecuperado = textQuantidade.getText().toString();
+
+        if ((valorRecuperado == null || valorRecuperado.equals(""))
+                && (quantidadeRecuperado == null || quantidadeRecuperado.equals(""))) {
             Toast.makeText(getApplicationContext(), "Digite um valor primeiro.", Toast.LENGTH_SHORT).show();
-        }
-        else {
+        } else {
             //converter String para double
             double valorDigitado = Double.parseDouble(valorRecuperado);
+            int quantidadeP = Integer.parseInt(quantidadeRecuperado);
 
             //calcula gorjeta total
-            double gorjeta = valorDigitado * (porcentagem/100);
+            double gorjeta = valorDigitado * (porcentagem / 100);
             double total = gorjeta + valorDigitado;
+
+            //calcula valor dividido
+            double totalDivido = total/quantidadeP;
 
             textGorjeta.setText("R$ " + gorjeta);
             textTotal.setText("R$ " + total);
+            textDividido.setText("R$ " + totalDivido);
         }
     }
 }
