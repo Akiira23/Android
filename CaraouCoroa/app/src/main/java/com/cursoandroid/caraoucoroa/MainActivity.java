79fd57daee47db2d43cb22b57e7b7725d11c7c30
Andre Akira <aah950@hotmail.com>
2022-03-30 11:49:56 -0300

    alteracoes no cara ou coroa
    contador de resultados de cara ou coroa

--- CaraouCoroa/app/src/main/java/com/cursoandroid/caraoucoroa/MainActivity.java
+++ CaraouCoroa/app/src/main/java/com/cursoandroid/caraoucoroa/MainActivity.java
@@ -6,12 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button buttonJogar;
+    private TextView textCara, textCoroa;
+    private int countCara = 0, countCoroa = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +22,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         buttonJogar = findViewById(R.id.buttonJogar);
+        textCara = findViewById(R.id.textCara);
+        textCoroa = findViewById(R.id.textCoroa);
+
 
         buttonJogar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
-
                 int num = new Random().nextInt(2);
 
                 intent.putExtra("numero", num);
                 startActivity(intent);
+                if (num == 0)
+                    countCara++;
+                else
+                    countCoroa++;
+                textCara.setText(String.valueOf(countCara));
+                textCoroa.setText(String.valueOf(countCoroa));
 
             }
         });
