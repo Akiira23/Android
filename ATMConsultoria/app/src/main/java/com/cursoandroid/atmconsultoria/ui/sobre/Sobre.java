1d4577d7f3f2faf8846f999f4842787a53567160
Andre Akira <aah950@hotmail.com>
2022-04-07 11:07:53 -0300

    Criando página sobre e finalizado o app
    finalizado o #App7

--- ATMConsultoria/app/src/main/java/com/cursoandroid/atmconsultoria/ui/sobre/Sobre.java
+++ ATMConsultoria/app/src/main/java/com/cursoandroid/atmconsultoria/ui/sobre/Sobre.java
@@ -11,6 +11,9 @@ import android.view.ViewGroup;
 
 import com.cursoandroid.atmconsultoria.R;
 
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -25,8 +28,33 @@ public class Sobre extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String descricao = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Etiam sodales leo vel augue egestas, id sollicitudin est tincidunt. Mauris non " +
+                "consequat magna, quis pretium nunc. In faucibus metus a ornare elementum.";
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sobre, container, false);
+        //return inflater.inflate(R.layout.fragment_sobre, container, false);
+
+        Element versao = new Element();
+        versao.setTitle("versao 1.0");
+
+       return new AboutPage(getActivity())
+               .setImage(R.drawable.logo)
+               .setDescription(descricao)
+
+               .addGroup("Entre em contato")
+               .addEmail("atendimento@atm.com.br", "Envie um Email")
+               .addWebsite("google.com", "Acesse nosso site")
+               .addGroup("Redes Sociais")
+               .addFacebook("the.medy", "Facebook")
+               .addInstagram("the.medy","Instagram")
+               .addTwitter("the.medy", "Twitter")
+               .addYoutube("the.medy", "YouTube")
+               .addGitHub("the.medy", "GitHub")
+               .addPlayStore("com.facebook.katana", "Download App")
+
+               .addItem(versao)
+               
+                .create();
     }
 
 }
